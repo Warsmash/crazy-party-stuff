0fb8090b6ac9b822b5a88718a38c596e74caed03
Aicha Diagne <awdstrategy@gmail.com>
2022-05-03 20:09:16 +0200

    added the user foreign key in attractions table and created the attractions controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_30_154706) do
+ActiveRecord::Schema.define(version: 2022_05_03_170533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,10 @@ ActiveRecord::Schema.define(version: 2022_04_30_154706) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_attractions_on_user_id"
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", default: "", null: false
@@ -36,4 +38,5 @@ ActiveRecord::Schema.define(version: 2022_04_30_154706) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "attractions", "users"
 end
