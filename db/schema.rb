6a2405d06634d6654528665ff536871172199317
Bruno LE LAY <brunolelay2000@gmail.com>
2022-05-03 20:08:39 +0200

    attraction policy

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2022_04_30_154706) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", default: "", null: false
