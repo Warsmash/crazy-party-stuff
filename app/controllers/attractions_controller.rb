ca9bb4638a3840ef8e965297b288f3d8e8670aec
Aicha Diagne <awdstrategy@gmail.com>
2022-05-05 21:57:10 +0200

    beginning of index + changement du nombre de caractères min

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,5 +1,7 @@
 class AttractionsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+  
   def index
-    @attractions = Attraction.all
+    @attractions = policy_scope(Attraction).order(created_at: :desc)
   end
 end
