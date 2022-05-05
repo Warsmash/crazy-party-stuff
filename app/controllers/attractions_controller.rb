188bee566971f22b52fd01bc9ea02360e66303dd
Bruno LE LAY <brunolelay2000@gmail.com>
2022-05-05 21:17:36 +0200

    create test

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -2,4 +2,21 @@ class AttractionsController < ApplicationController
   def index
     @attractions = policy_scope(Attraction).order(created_at: :desc)
   end
+
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.new(attraction_params)
+    @user = User.find(params[:user_id])
+    @attraction.user = @user
+    @attraction.save
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:content)
+  end
 end
