c4f39993fb48759dc6dc05781bc4c93431d9a7b3
Bruno LE LAY <brunolelay2000@gmail.com>
2022-05-10 22:09:57 +0200

    search bar marche

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  include Pundit::Authorization
-  after_action :verify_authorized, except: [:show, :index], unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  # include Pundit::Authorization
+  # after_action :verify_authorized, except: [:show, :index], unless: :skip_pundit?
+  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
 
    # [...]
